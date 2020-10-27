b63c599b821a56b67ed939b489bc7a695fd695a8
Pedro_portatil <a16pednieper@inspedralbes.cat>
2020-10-27 12:20:36 +0100

    header_footer

---
+++ PHP/menu.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">    
+    <title>Menu</title>
+</head>
+<body>
+<?php include "header.php"; ?>  
+    <h1>Menu Cantina Pedralbes</h1>
+</body>
+<?php 
+include "footer.php";
+?>
+</html>
