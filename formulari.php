591f56228d3fb56de1f34c95f593a8899e8f4f7b
Gerard <a20gerarnoli@inspedralbes.cat>
2020-10-29 09:23:03 +0100

    afegir css a formulari i error

--- formulari.php
+++ formulari.php
@@ -1,5 +1,13 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+</head>
 <body>
+    
     <?php include "header.php";?>
 
     <br>
@@ -26,8 +34,13 @@
         <script src="js/formulari.js"></script>    
     </script>
 
+    <br>
+
     <?php include "footer.php";?>
-    
+
 </body>
 </html>
+
+
+
 
--- pagina_error.php
+++ pagina_error.php
@@ -1,7 +1,23 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/styles.css" type="text/css">
+</head>
+<body>
+    <?php include "header.php"; ?>
+        
+    <br><br>
 
-    if (isset($_COOKIE)) {
-        echo "Ja has fet una comanda avui, torna demà :-)";
-    }
-    
-?>
+    <?php if (isset($_COOKIE)) {echo "Ja has fet una comanda avui, torna demà :-)";} ?>
+
+    <br><br><br>
+
+    <?php include "footer.php";?>
+
+</body>
+</html>
+
+
