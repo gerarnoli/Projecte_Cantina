d639f3bf4049494df35c600673eb6a5f3c6d9c5b
Pedro_portatil <a16pednieper@inspedralbes.cat>
2020-10-29 08:54:36 +0100

    taula_menu

--- menu.php
+++ menu.php
@@ -10,7 +10,49 @@
 <?php include "header.php"; ?>  
     <h3 class="titol1">Menu Cantina Pedralbes</h3>  
 
-    
+    <table id="taula">
+    <thead>
+        <tr>
+            <th>Items</th>
+            <th>Preu</th>
+            <th>Quantitat</th>
+            <th>Total</th>            
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            
+            <td class="goods"><img src="images/1.jpg" alt=""/><span> Bocata </span></td>
+            <td class="price"> 20 </td>
+            <td class="count">
+            <span class="reduce"> - </span>
+            <input class="count-input" type="text" value="1"/><span class="add"> + </span></td>            
+                        
+        </tr>
+        <tr>            
+            <td class="goods"><img src="images/2.jpg" alt=""/><span> Cafe </span></td>
+            <td class="price"> 10 </td>
+            <td class="count"><span class="reduce"> - </span>
+            <input class="count-input" type="text" value="1"/><span class="add"> + </span></td>
+                        
+        </tr>
+        <tr>
+            <td class="goods"><img src="images/3.jpg" alt=""/><span> Donut </span></td>
+            <td class="price"> 50 </td>
+            <td class="count"><span class="reduce"> - </span>
+            <input class="count-input" type="text" value="1"/><span class="add"> + </span></td>
+                    
+        </tr>
+        <tr>
+            <td class="goods"><img src="images/4.jpg" alt=""/><span> Playstation </span></td>
+            <td class="price"> 600 </td>
+            <td class="count"><span class="reduce"> - </span>
+            <input class="count-input" type="text" value="1"/><span class="add"> + </span></td>
+                      
+        </tr>
+    </tbody>
+</table>
+    <!--
     <input type="checkbox" id="vehicle1" name="vehicle1" value="xorizo">
     <label for="vehicle1"> Bocata Xorizo</label><br>
     <div class='btn'> + </div>
@@ -20,9 +62,9 @@
 
     <input type="checkbox" id="vehicle1" name="vehicle1" value="coca">
     <label for="vehicle1"> CocaCola</label><br>
-
-    <button onclick="enviarPedido()">Enviar Pedido</button>
     
+    <button onclick="enviarPedido()">Enviar Pedido</button>
+    -->
       
     <script src="js/menu.js"></script>
 </form>
