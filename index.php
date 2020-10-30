de6e53d2c391827c88a9d4a473f64a57a8395400
a15anamatros <a15anamatros@inspedralbes.cat>
2020-10-30 12:16:23 +0100

    se sube archivo index

--- index.php
+++ index.php
@@ -1,18 +1,66 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="css/styles.css" type="text/css">
-    <title>DCantina Pedralbes</title>
+    <title>Cantina</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
 </head>
 <body>
-    <?php include "header.php";?>
+    <?php include("top.php") ?>
 
-    <button onclick="location.href='menu.php'">Menu</button>
-    <button onclick="location.href='pagina_error.php'">Admin</button>
+    <div class="container">
+    <h2>Carousel Example</h2>
+    <div id="myCarousel" class="carousel slide" data-ride="carousel">
+        <!-- Indicators -->
+        <ol class="carousel-indicators">
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="1"></li>
+        <li data-target="#myCarousel" data-slide-to="2"></li>
+        </ol>
 
+        <!-- Wrapper for slides -->
+        <div class="carousel-inner">
 
-    <?php include "footer.php";?>    
+            <div class="item active">
+                <img src="la.jpg" alt="Los Angeles" style="width:100%;">
+                <div class="carousel-caption">
+                <h3>Los Angeles</h3>
+                <p>LA is always so much fun!</p>
+                </div>
+            </div>
+
+            <div class="item">
+                <img src="chicago.jpg" alt="Chicago" style="width:100%;">
+                <div class="carousel-caption">
+                <h3>Chicago</h3>
+                <p>Thank you, Chicago!</p>
+                </div>
+            </div>
+            
+            <div class="item">
+                <img src="ny.jpg" alt="New York" style="width:100%;">
+                <div class="carousel-caption">
+                <h3>New York</h3>
+                <p>We love the Big Apple!</p>
+                </div>
+            </div>
+        </div>
+
+        <!-- Left and right controls -->
+        <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#myCarousel" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+    </div>
+
+    <?php include("down.php") ?>
 </body>
 </html>
