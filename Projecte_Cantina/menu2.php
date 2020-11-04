92edf9ff74f971ff20878cbebfbf2a97ae556620
Peter Casa <a16pednieper@inspedralbes.cat>
2020-11-04 12:24:24 +0100

    canvis_menu2

--- Projecte_Cantina/menu2.php
+++ Projecte_Cantina/menu2.php
@@ -9,154 +9,70 @@
     </style>
     <script type="text/javascript" src="js/script.js"></script>
 
-    <style>  
+    <style> 
 
-* {
-    margin: 0;
-    padding: 0;
-    list-style: none;
-    font-family: FunctionRegular;
-}
+    .productos{
+        float: left;
+        display: flex;
+        border: 1px solid red;
+    }
 
-header{
-    padding: 0px 40px;
-    background-color: #383838;
-    color: white;
-    margin-bottom: 40px;
-}
-/* Este es un div contiene toda la pagia web, su padre es el body */
-#bodyContenedor {
-    width: 1024px;
-    margin: 0 auto;
-    background-color: white;
-}
+    .producto{
+        margin: 10px;
+        padding: 10px;
+        border: 1px solid blue;
+    }
 
-/* Este es un div contiene cada producto por separado */
+    .listafinal{
+        float: left;
+        margin: 10px;        
+    }
 
-.producto {
-    float: left;
-    margin-right: 40px;
-    margin-bottom: 15px;
-    text-align: center;
-    font-size: 100%;
-    cursor: pointer;
-}
 
-.producto img {
-    width: 100px;
-}
 
-.Comprar {
-    color: green;
-}
 
-#carritoDeCompras {
-    clear: left;
-    padding-bottom: 15px;
-    border-top: 1px solid #ccc;
-}
 
-#carritoDeCompras div {
-    padding-top: 15px;
-    padding-bottom: 15px;
-    border-bottom: 1px solid #ccc;
-}
-
-#carritoDeCompras img {
-    float: left;
-    width: 70px;
-    margin-right: 10px;
-}
-
-#carritoDeCompras h2 h3 {
-    font-size: 150%;
-}
-
-#carritoDeCompras .cantidad {
-    float: right;
-    line-height: 25px;
-}
-
-#carritoDeCompras button {
-    display: inline-block;
-    float: right;
-    margin: 0 10px;
-    padding: 5px 15px;
-    text-align: center;
-    text-decoration: none;
-    color: white;
-    border: none;
-    background-color: #555555;
-    font-size: 13px;
-}
-
-#carritoDeCompras .mas {
-    margin-right: 40%;
-}
-
-#carritoDeCompras button:focus {
-    outline: 0px;
-}
-
-#carritoDeCompras button:hover {
-    box-shadow: 0 5px 5px 0 rgba(0, 0, 0, 0.24), 0 1px 1px 0 rgba(0, 0, 0, 0.19);
-}
-
-#precioTotal {
-    float: right;
-    background-color: white;
-    font-size: 200%;
-}
-</style>
+    </style>
 
 </head>    
-    <body>      
-        <div id="bodyContenedor">
-            <div id="productoContenedor">
-                <div class="producto" data-idproducto="Refresco" data-img="images/coca_cola.jpg"
-                    data-des="Coca-Cola bien fresquita" data-precio="10" data-cantidad="0"> 
-                    <img src="images/coca_cola.jpg" alt="CocaCola">
-                    <h2>Refresco</h2>
-                    <p>10 €</p>
-                    <p class="Comprar">Comprar</p>
+    <body>
+            <div id="productos" class="productos">
+ 
+                <div id="producto" class="producto"> <img src="images/bocata.jpg" width="200" height="200">
+                <p> Bocata </p>
+                <p> 5 €</p>  
+                <input type="number" value="" max="5">              
                 </div>
-                <div class="producto" data-idproducto="Bocata" data-img="images/bocata.jpg"
-                    data-des="B" data-precio="12" data-cantidad="0">
-                    <img src="images/bocata.jpg" alt="Bocata">
-                    <h2> Bocata </h2>
-                    <p> 12 €</p>
-                    <p class="Comprar">Comprar</p>
+
+                <div id="producto" class="producto"> <img src="images/croissant.jpg" width="200" height="200">
+                <p> Croissant </p>
+                <p> 3 €</p>
+                <input type="number" value="" max="5">  
                 </div>
-                <div class="producto" data-idproducto="Croissant" data-img="images/croissant.jpg" data-des="cruasant dulce"
-                    data-precio="15" data-cantidad="0"> <img src="images/croissant.jpg" alt="croissant">
-                    <h2>Croissant</h2>
-                    <p> 15 €</p>
-                    <p class="Comprar">Comprar</p>
+
+                <div id="producto" class="producto"> <img src="images/coca_cola.jpg" width="200" height="200">
+                <p> Coca Cola</p>
+                <p> 2 €</p> 
+                <input type="number" value="" max="5">   
                 </div>
-                <div class="producto" data-idproducto="Paella" data-img="images/paella.jpg"
-                    data-des="paella valensiana" data-precio="9" data-cantidad="0"> <img src="images/paella.jpg"
-                        alt="Paella">
-                    <h2>Paella</h2>
-                    <p>100 €</p>
-                    <p class="Comprar">Comprar</p>
+
+                <div id="producto" class="producto"> <img src="images/taza_cafe.jpg" width="200" height="200">
+                <p> Cafe </p>
+                <p> 5 €</p>  
+                <input type="number" value="" max="5">  
                 </div>
-                <!-- 
-                <div class="producto" data-idproducto="Chokis" data-img="img/Chokis.jpg"
-                    data-des="Galletas con chispas de chocolate" data-precio="12" data-cantidad="0">
-                    <img src="img/Chokis.jpg" alt="Chokis">
-                    <h2>Chokis</h2>
-                    <p>$12</p>
-                    <p class="Comprar">Comprar</p>
-                </div>  
-                -->        
+           
+
+                
+
+
             </div>
 
-            <div id="carritoDeCompras"> </div>
-            <h2 id=precioTotal>A Pagar: $0</h2>
-        </div>
-
-
-
-            <button onclick="location.href='formulari.php'" class="checkout">CheckOut</button>           
+            <div id="listafinal" class="listafinal"> 
+                <h2 id=preciofinal> A Pagar </h2>
+                <button onclick="location.href='formulari.php'" class="checkout">CheckOut</button>   
+            </div>
+            
+                    
     </body>
 </html>
