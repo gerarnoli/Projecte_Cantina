b340220bf965ca2ea13e9dbe23146d2396e3bafd
gerarnoli <a20gerarnoli@inspedralbes.cat>
2020-11-05 19:01:08 +0100

    script

--- js/script.js
+++ js/script.js
@@ -16,6 +16,8 @@ function tanquemLlista() {
     return (location.href='formulari.php');
 }
 
+/*
 function actualitzarCarro () {
     document.getElementById("listafinal").innerHTML = localStorage.getItem("comandes");
 }
+*/
